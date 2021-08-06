85ca078b9979f5f87396838e6c2c7abdd7f8837f
abir-imtiaz <abir19imtiaz@gmail.com>
2021-08-07 01:34:34 +0600

    initial push

---
+++ sendText.js
@@ -0,0 +1,23 @@
+function text() {
+  alert(username);
+  //   const phone = document.getElementById("phone").value;
+  //   const msg = document.getElementById("message").value;
+
+  //   var data = "";
+  //   var xhr = new XMLHttpRequest();
+  //   xhr.withCredentials = true;
+  //   xhr.addEventListener("readystatechange", function () {
+  //     if (this.readyState === 4) {
+  //       console.log(this.responseText);
+  //     }
+  //   });
+  //   xhr.open(
+  //     "POST",
+  //     "http://66.45.237.70/api.php?username=abir.imtiaz&password=0$Ecstassy&number=" +
+  //       phone +
+  //       "&message=" +
+  //       encodeURIComponent(msg)
+  //   );
+  //   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  //   xhr.send(data);
+}
